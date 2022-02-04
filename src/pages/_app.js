b153e55550ab09792409ef49169a9dbfe8c1b87b
wesley_wisch <wesley_wisch@hotmail.com>
2022-02-04 10:54:50 -0300

    Estrutura mais os estilos prontos

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { GlobalStyle } from '../styles/GlobalStyles';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,55 @@
+import Arrow from '../assets/icon-arrow.svg';
+
+import { Container, SearchInfos, SearchSection, MapContainer } from '../styles/HomeStyles';
+
 export default function Home() {
+  const searchInfos = [
+    {
+      title: 'IP Address',
+      description: '192.000.0.000',
+    },
+    {
+      title: 'Location',
+      description: 'Rio de janeiro, BR',
+      br: '10001'
+    },
+    {
+      title: 'Timezone',
+      description: 'UTC',
+    },
+    {
+      title: 'ISP',
+      description: 'SpaceX',
+    },
+  ];
+
   return (
-    <h1>Home</h1>
+    <Container>
+      <SearchSection results={true}>
+        <h2>IP address Tracker</h2>
+
+        <div>
+          <input type="text" placeholder="Search for any IP address or domain" />
+          <button><Arrow /></button>
+        </div>
+
+        <SearchInfos>
+          <ul>
+            {searchInfos.map((item, key) => (
+              <>
+                <li key={key}>
+                  <div>
+                    <strong>{item.title}</strong>
+                    <p>{item.description} {item.br ? item.br : ''}</p>
+                  </div>
+                </li>
+              </>
+            ))}
+          </ul>
+        </SearchInfos>
+      </SearchSection>
+
+      <MapContainer />
+    </Container>
   )
 }
