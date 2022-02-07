ce63e1ccf69babae4d958ada515eacc0aa579949
wesley_wisch <wesley_wisch@hotmail.com>
2022-02-07 13:11:18 -0300

    Integrando a api do mapa, e exibindo em tela

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,42 @@
+import { useEffect, useState } from 'react';
 import Arrow from '../assets/icon-arrow.svg';
 
 import { Container, SearchInfos, SearchSection, MapContainer } from '../styles/HomeStyles';
 
 export default function Home() {
-  const searchInfos = [
-    {
-      title: 'IP Address',
-      description: '192.000.0.000',
-    },
-    {
-      title: 'Location',
-      description: 'Rio de janeiro, BR',
-      br: '10001'
-    },
-    {
-      title: 'Timezone',
-      description: 'UTC',
-    },
-    {
-      title: 'ISP',
-      description: 'SpaceX',
-    },
-  ];
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState({});
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const ipAddress = process.env.NEXT_PUBLIC_IP_ADDRESS;
+
+  useEffect(() => {
+    async function getInitialData() {
+      try {
+        setLoading(true);
+
+        const response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}`)
+        .then(response => response.json())
+        .then(data => setResults(data));
+
+        if (response.status !== 200) {
+          throw new Error();
+        };
+
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getInitialData();
+  }, []);
+
 
   return (
     <Container>
-      <SearchSection results={true}>
+      <SearchSection results={results.location}>
         <h2>IP address Tracker</h2>
 
         <div>
@@ -33,20 +44,39 @@ export default function Home() {
           <button><Arrow /></button>
         </div>
 
-        <SearchInfos>
-          <ul>
-            {searchInfos.map((item, key) => (
-              <>
-                <li key={key}>
-                  <div>
-                    <strong>{item.title}</strong>
-                    <p>{item.description} {item.br ? item.br : ''}</p>
-                  </div>
-                </li>
-              </>
-            ))}
-          </ul>
-        </SearchInfos>
+        {results?.location && (
+          <SearchInfos>
+            <ul>
+              <li>
+                <div>
+                  <strong>IP Address</strong>
+                  <p>192.000.0.000</p>
+                </div>
+              </li>
+
+              <li>
+                <div>
+                  <strong>Location</strong>
+                  <p>{`${results.location.city}, ${results.location.country}`}<br /> {results.location.region}</p>
+                </div>
+              </li>
+
+              <li>
+                <div>
+                  <strong>Timezone</strong>
+                  <p>UTC {results.location.timezone}</p>
+                </div>
+              </li>
+
+              <li>
+                <div>
+                  <strong>ISP</strong>
+                  <p>{results.isp}</p>
+                </div>
+              </li>
+            </ul>
+          </SearchInfos>
+        )}
       </SearchSection>
 
       <MapContainer />
