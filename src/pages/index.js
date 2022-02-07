508ef504d8c0003d3b185d9863327ca9a041126b
wesley_wisch <wesley_wisch@hotmail.com>
2022-02-07 13:39:27 -0300

    Adicionando a função de buscar por ip ou dominio

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,19 @@ import { Container, SearchInfos, SearchSection, MapContainer } from '../styles/H
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
+  const [ipAddress, setIpAddress] = useState('');
   const [results, setResults] = useState({});
 
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  const ipAddress = process.env.NEXT_PUBLIC_IP_ADDRESS;
 
   useEffect(() => {
     async function getInitialData() {
       try {
         setLoading(true);
 
-        const response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}`)
-        .then(response => response.json())
-        .then(data => setResults(data));
+        const response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}`)
+          .then(response => response.json())
+          .then(data => setResults(data));
 
         if (response.status !== 200) {
           throw new Error();
@@ -33,6 +33,42 @@ export default function Home() {
     getInitialData();
   }, []);
 
+  async function handleSearchInApi() {
+    if (!ipAddress) {
+      alert('Por favor escreva algum IP!');
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      // Verificação (regex) se é realmente um IP.
+      if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipAddress)) {
+        const response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}`)
+          .then(response => response.json())
+          .then(data => setResults(data));
+
+        if (response.status !== 200) {
+          throw new Error();
+        };
+      } else {
+        // Verificação que se não é um ip então é um domínio.
+        const response = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&domain=${ipAddress}`)
+          .then(response => response.json())
+          .then(data => setResults(data));
+
+        if (response.status !== 200) {
+          throw new Error();
+        };
+      }
+
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
 
   return (
     <Container>
@@ -40,8 +76,13 @@ export default function Home() {
         <h2>IP address Tracker</h2>
 
         <div>
-          <input type="text" placeholder="Search for any IP address or domain" />
-          <button><Arrow /></button>
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+            onChange={(e) => setIpAddress(e.target.value)}
+            value={ipAddress}
+          />
+          <button onClick={handleSearchInApi}><Arrow /></button>
         </div>
 
         {results?.location && (
@@ -50,14 +91,14 @@ export default function Home() {
               <li>
                 <div>
                   <strong>IP Address</strong>
-                  <p>192.000.0.000</p>
+                  <p>{results.ip}</p>
                 </div>
               </li>
 
               <li>
                 <div>
                   <strong>Location</strong>
-                  <p>{`${results.location.city}, ${results.location.country}`}<br /> {results.location.region}</p>
+                  <p>{`${results.location.region}, ${results.location.country}`}<br /></p>
                 </div>
               </li>
 
